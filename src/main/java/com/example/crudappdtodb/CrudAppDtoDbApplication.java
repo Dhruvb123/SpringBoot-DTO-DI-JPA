3ca8f015f912bd8877608202cd4af84ecc623cee
Dhruv Bheda <dhruvbheda009@gmail.com>
2026-01-01 21:36:29 +0530

    Init Commit

---
+++ src/main/java/com/example/crudappdtodb/CrudAppDtoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudappdtodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAppDtoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudAppDtoDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crudappdtodb/CrudAppDtoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudappdtodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAppDtoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
