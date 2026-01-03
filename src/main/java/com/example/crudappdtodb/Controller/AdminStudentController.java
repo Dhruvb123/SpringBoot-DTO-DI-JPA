6cd5b72159b0405c0c1b62fe46d67d5731ab7d93
Dhruv Bheda <dhruvbheda009@gmail.com>
2026-01-03 18:08:32 +0530

    Complete Controller Endpoints

--- src/main/java/com/example/crudappdtodb/Controller/AdminStudentController.java
+++ src/main/java/com/example/crudappdtodb/Controller/AdminStudentController.java
@@ -1,5 +1,6 @@
 package com.example.crudappdtodb.Controller;
 
+import com.example.crudappdtodb.DTO.StudentDTO;
 import com.example.crudappdtodb.Service.AdminStudentService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,35 @@ import java.util.List;
 @RequestMapping("/api/admin/students")
 public class AdminStudentController {
 
+    private final AdminStudentService service;
 
+    public AdminStudentController(
+            @Qualifier("admin") AdminStudentService service) {
+        this.service = service;
+    }
 
+    @PostMapping
+    public StudentDTO create(@RequestBody StudentDTO dto) {
+        return service.create(dto);
+    }
+
+    @GetMapping
+    public List<StudentDTO> getAll() {
+        return service.getAll();
+    }
+
+    @PutMapping("/{id}")
+    public StudentDTO update(@PathVariable Long id, @RequestBody StudentDTO dto) {
+        return service.update(id, dto);
+    }
+
+    @PatchMapping("/{id}")
+    public StudentDTO patch(@PathVariable Long id, @RequestBody StudentDTO dto) {
+        return service.patch(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
 }
--- src/main/java/com/example/crudappdtodb/Controller/StudentController.java
+++ src/main/java/com/example/crudappdtodb/Controller/StudentController.java
@@ -1,5 +1,7 @@
 package com.example.crudappdtodb.Controller;
 
+import com.example.crudappdtodb.DTO.StudentDTO;
+import com.example.crudappdtodb.Service.PublicStudentService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -7,5 +9,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/public/students")
 public class StudentController {
+    private final PublicStudentService service;
 
+    public StudentController(PublicStudentService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<StudentDTO> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public StudentDTO getById(@PathVariable Long id) {
+        return service.getById(id);
+    }
 }
